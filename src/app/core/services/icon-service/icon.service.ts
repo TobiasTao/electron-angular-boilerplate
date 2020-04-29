d9cfab4237dd624f213d3d12fd91c24d873d60d6
tyc <952530904@qq,com>
2020-04-29 13:05:32 +0800

    :tada: Initial commit

--- src/app/core/services/icon-service/icon.service.ts
+++ src/app/core/services/icon-service/icon.service.ts
@@ -1,29 +1,21 @@
-import { MatIconRegistry } from "@angular/material/icon";
-import { Injectable } from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
-import { icons } from "./icons";
+import { MatIconRegistry } from '@angular/material/icon';
+import { Injectable } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { icons } from './icons';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class IconService {
-  constructor(
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
-  ) {}
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {}
 
   public registerIcons(): void {
-    this.loadIcons(icons, "../../../assets/icons");
+    this.loadIcons(icons, 'assets/icons');
   }
 
   private loadIcons(iconKeys: string[], iconUrl: string): void {
     iconKeys.forEach((key) => {
-      this.matIconRegistry.addSvgIcon(
-        key,
-        this.domSanitizer.bypassSecurityTrustResourceUrl(
-          `${iconUrl}/${key}.svg`
-        )
-      );
+      this.matIconRegistry.addSvgIcon(key, this.domSanitizer.bypassSecurityTrustResourceUrl(`${iconUrl}/${key}.svg`));
     });
   }
 }
